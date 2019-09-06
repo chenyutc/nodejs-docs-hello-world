43373cf249d4e9fc9cc4f12c46e8a637a1f58c0b
chenyutc <54970863+chenyutc@users.noreply.github.com>
2019-09-06 14:23:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World! Test jenkins");
+    response.end("Hello World! Test jenkins\n Line 2");
 });
 
 const port = process.env.PORT || 1337;
